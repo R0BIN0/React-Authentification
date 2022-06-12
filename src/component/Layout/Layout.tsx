e0613e241d3bfde00cce243e38c49d4602ebd7a7
Robin <rob.blanquart02100@gmail.com>
2022-06-13 01:18:05 +0200

    [ADD] make right UI part with responsive in Layout component

--- src/component/Layout/Layout.tsx
+++ src/component/Layout/Layout.tsx
@@ -6,7 +6,6 @@ import "./Layout.css";
 
 // Images
 import Logo from "../../assets/images/Logo.png";
-import CurvedLines from "../../assets/images/curved-lines.png";
 
 export default function Layout() {
   return (
@@ -24,33 +23,37 @@ export default function Layout() {
         </div>
       </div>
       <div className="container-grid-right">
-        <h1 className="title-container">Set up your Hub account</h1>
-        <p className="subtitle-container">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ab
-          velit accusantium? Voluptate, inventore non.
-        </p>
-        <form>
-          <div className="input-container">
-            <input type="text" id="name" placeholder="Nom" />
-            <input type="text" id="name" placeholder="Nom" />
-            <input type="text" id="name" placeholder="Nom" />
-            <input type="text" id="name" placeholder="Nom" />
-          </div>
-          <div className="terms-container">
-            <input type="checkbox" />
+        <div className="container-grid-right-wrapper">
+          <h1 className="title-container">Set up your Hub account.</h1>
+          <p className="subtitle-container">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ab
+            velit accusantium? Voluptate, inventore non.
+          </p>
+          <form>
+            <div className="input-container">
+              <input type="text" id="name" placeholder="Nom" />
+              <input type="text" id="name" placeholder="Nom" />
+              <input type="text" id="name" placeholder="Nom" />
+              <input type="text" id="name" placeholder="Nom" />
+              <input type="text" id="name" placeholder="Nom" />
+            </div>
+            <div className="terms-container">
+              <input type="checkbox" />
+              <p>
+                I agree with Hub's <a href="/">Terms</a>,{" "}
+                <a href="/">Privacy Policy</a> and <a href="/">E-Sign</a>{" "}
+                consent.
+              </p>
+            </div>
+            <div className="button-container">
+              <button type="submit">Continue</button>
+            </div>
+          </form>
+          <div className="switch-page">
             <p>
-              I agree with Hub's <a href="/">Terms</a>,{" "}
-              <a href="/">Privacy Policy</a> and <a href="/">E-Sign</a> consent.
+              Already have an account ? <a href="/">Sign in</a>
             </p>
           </div>
-          <div className="button-container">
-            <button type="submit">Continue</button>
-          </div>
-        </form>
-        <div className="switch-page">
-          <p>
-            Already have an account ? <a href="/">Sign in</a>
-          </p>
         </div>
       </div>
     </div>
