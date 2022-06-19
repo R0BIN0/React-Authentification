fddb808bceaa62f9d3db5ee0858865cac9d884d9
Robin <rob.blanquart02100@gmail.com>
2022-06-19 14:02:49 +0200

    [ADD] Private Content page UI added

--- src/pages/PrivateContent/PrivateContent.tsx
+++ src/pages/PrivateContent/PrivateContent.tsx
@@ -1,9 +1,16 @@
-// General
-import React from "react";
-
 // Styles
 import "./PrivateContent.css";
 
+// Components
+import Button from "../../component/Button/Button";
+
 export default function PrivateContent() {
-  return <div>PrivateContent</div>;
+  return (
+    <div className="private-container">
+      <p>
+        Vous êtes connecté sous le pseudo : <strong>Robin</strong>
+      </p>
+      <Button title="Se déconnecter" />
+    </div>
+  );
 }
