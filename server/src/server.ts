ae29a266ac97c8de26e1d4712e9887128da59ddc
Robin <rob.blanquart02100@gmail.com>
2022-06-20 13:25:10 +0200

    [ADD] app.ts and environnement variables added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,6 @@
-console.log("Hello World");
+const rootApp = require("./app");
+require("dotenv").config({ path: "server/src/config/config.env" });
+
+rootApp.listen(process.env.PORT, () => {
+  console.log(`Server started on port : ${process.env.PORT}`);
+});
